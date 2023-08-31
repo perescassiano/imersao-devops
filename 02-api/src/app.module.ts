25329a98b0efecea43438838e6183a59bdc4ec43
perescassiano <cassianoperes89@gmail.com>
2023-08-31 17:14:59 -0300

    First commit

---
+++ 02-api/src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AssetsModule } from './assets/assets.module';
+import { WalletsModule } from './wallets/wallets.module';
+import { OrdersModule } from './orders/orders.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { SimulateAssetsPriceCommand } from './simulate-assets-price.command';
+import { PrometheusModule } from "@willsoto/nestjs-prometheus";
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    PrismaModule,
+    AssetsModule,
+    WalletsModule,
+    OrdersModule,
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+    PrometheusModule.register({
+      defaultLabels: {
+        version: "v2",
+      },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService, SimulateAssetsPriceCommand],
+})
+export class AppModule {}
